1ac066536b9f1622737ba3832cef499f96794382
Emile den Tex <emile@digitalmethods.net>
2016-05-11 16:36:58 +0200

    Use tcat_captured_phrases table to export ratelimit estimates per bin and subquery (implements #150)

--- analysis/mod.ratelimits.php
+++ analysis/mod.ratelimits.php
@@ -71,7 +71,7 @@ require_once __DIR__ . '/common/CSV.class.php';
         $csv = new CSV($filename, $outputformat);
 
         // write header
-        $header = "interval,querybin,datetime,ratelimited (estimate)";
+        $header = "interval,querybin,datetime,tweets ratelimited (estimate)";
         $csv->writeheader(explode(',', $header));
 
         $sqlInterval = sqlInterval(); $sqlSubset = sqlSubset();
@@ -83,18 +83,84 @@ require_once __DIR__ . '/common/CSV.class.php';
          *                                                      total unique tweets with matches    (B)
          */
 
-
-        $sql_query_a = "";        // TODO: get from tcat_error_ratelimit
+        $sqlIntervalForRL = str_replace("t.created_at", "start", $sqlInterval);
+        $sql_query_a = "SELECT SUM(tweets) as ratelimited, $sqlIntervalForRL FROM tcat_error_ratelimit WHERE start >= '" . $esc['datetime']['startdate'] . "' AND end <= '" . $esc['datetime']['enddate'] . "' $sqlGroup";
 
         // This query retrieves the total unique tweets captured, grouped by the requested interval (hourly, daily, ...)
         $sql_query_b = "SELECT COUNT(distinct(t.tweet_id)) AS cnt, $sqlInterval FROM tcat_captured_phrases t $sqlSubset $sqlGroup";
 
         // Notice: we do need to do a INNER JOIN on the querybin table here (to match phrase_id to querybin_id), but I'm assuming this is not expensive now because that table is tiny?
-        $sql_query_c = "SELECT COUNT(distinct(t.tweet_id)) AS cnt, $sqlInterval FROM tcat_captured_phrases t INNER JOIN tcat_query_bins_phrases qbp ON t.phrase_id = qbp.phrase_id $sqlSubset AND qbp.querybin_id = $bin_id  $sqlGroup";
+        $sql_query_c = "SELECT COUNT(distinct(t.tweet_id)) AS cnt, $sqlInterval FROM tcat_captured_phrases t INNER JOIN tcat_query_bins_phrases qbp ON t.phrase_id = qbp.phrase_id $sqlSubset AND qbp.querybin_id = $bin_id $sqlGroup";
 
+/*
         echo "Query A:"; echo "<pre>"; print_r($sql_query_a); echo "</pre><br>";
         echo "Query B:"; echo "<pre>"; print_r($sql_query_b); echo "</pre><br>";
         echo "Query C:"; echo "<pre>"; print_r($sql_query_c); echo "</pre><br>";
+*/
+
+        $fullresults = array();
+
+        // Get ratelimits (query A)
+
+        $rec = mysql_query($sql_query_a);
+        while ($res = mysql_fetch_assoc($rec)) {
+            if (!array_key_exists($res['datepart'], $fullresults)) {
+                $fullresults[$res['datepart']] = array();
+            }
+            $fullresults[$res['datepart']]['ratelimited'] = $res['ratelimited'];
+        }
+
+        // Get the total unique phrases with matches (query B)
+
+        $rec = mysql_query($sql_query_b);
+        while ($res = mysql_fetch_assoc($rec)) {
+            if (!array_key_exists($res['datepart'], $fullresults)) {
+                $fullresults[$res['datepart']] = array();
+            }
+            $fullresults[$res['datepart']]['totalphrases'] = $res['cnt'];
+        }
+        
+        // Get the measured phrases per bin (query C)
+
+        $rec = mysql_query($sql_query_c);
+        while ($res = mysql_fetch_assoc($rec)) {
+            if (!array_key_exists($res['datepart'], $fullresults)) {
+                $fullresults[$res['datepart']] = array();
+            }
+            $fullresults[$res['datepart']]['measuredbin'] = $res['cnt'];
+        }
+
+        foreach ($fullresults as $datepart => $row) {
+            if (!array_key_exists('ratelimited', $row)) {
+                $row['ratelimited'] = 0;
+            }
+            if (!array_key_exists('measuredbin', $row)) {
+                $row['measuredbin'] = 0;
+            }
+            if (!array_key_exists('totalphrases', $row)) {
+                if ($row['measuredbin'] > 0) {
+                    // This would clearly signify a bug. We have a totalphrases < measured phrases?
+                    // To avoid a divide by zero, exit.
+                    echo "BUG detected in resultset at datepart '$datepart'<br/>Now dumping row structure.<br/>";
+                    echo "<pre>";
+                    print_r($row);          
+                    echo "</pre>";
+                    exit();
+                }
+            }
+
+            // Now: calculate the estimate using our formula
+            $estimate = $row['ratelimited'] * $row['measuredbin'] / $row['totalphrases'];
+
+            $csv->newrow();
+            $csv->addfield($interval);
+            $csv->addfield($esc['mysql']['dataset']);
+            $csv->addfield($datepart);
+            $csv->addfield($estimate);
+            $csv->writerow();
+        }
+
+        $csv->close();
 
         echo '<fieldset class="if_parameters">';
         echo '<legend>Your File</legend>';
