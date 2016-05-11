339571080ee257be89a8c0bca1557f911bb137bb
Emile <emile@tcat5.temlab>
2016-05-11 16:58:58 +0200

    We need INSERT DELAYED here, or captured will lockup if upgrade is in progress.

--- capture/common/functions.php
+++ capture/common/functions.php
@@ -1876,7 +1876,7 @@ function insert_captured_phrase_ids($captured_phrase_ids) {
     if (count($captured_phrase_ids) > 3) {
         $moresets = count($captured_phrase_ids) / 3 - 1;
     }
-    $sql = "INSERT " . (DISABLE_INSERT_IGNORE ? "" : 'IGNORE') . " INTO tcat_captured_phrases ( tweet_id, phrase_id, created_at ) VALUES ( ?, ?, ? )" . str_repeat(", (?, ?, ?)", $moresets);
+    $sql = "INSERT DELAYED " . (DISABLE_INSERT_IGNORE ? "" : 'IGNORE') . " INTO tcat_captured_phrases ( tweet_id, phrase_id, created_at ) VALUES ( ?, ?, ? )" . str_repeat(", (?, ?, ?)", $moresets);
     $h = $dbh->prepare($sql);
     for ($i = 0; $i < count($captured_phrase_ids); $i++) {
         // bindParam() expects its first parameter ( index of the ? placeholder ) to start with 1
